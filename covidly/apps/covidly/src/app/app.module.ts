c9692492c7b9bd51946ff84c37fbd52d3b846028
jadewick <darrel.gaming.hk@gmail.com>
2020-03-23 21:42:25 -0500

    adding covid-graph component

--- covidly/apps/covidly/src/app/app.module.ts
+++ covidly/apps/covidly/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CovidGraphComponent } from './covid-graph/covid-graph.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CovidGraphComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ covidly/apps/covidly/src/app/covid-graph/covid-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'covidly-covid-graph',
+  templateUrl: './covid-graph.component.html',
+  styleUrls: ['./covid-graph.component.css']
+})
+export class CovidGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
